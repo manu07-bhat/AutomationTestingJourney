1158338b8d268b19e41013d82ee57031b7a8f886
manoj a <manubhat701@gmail.com>
2025-01-14 04:11:47 +0530

    DAY 2

--- src/Java_Concepts/Practice_1.java
+++ src/Java_Concepts/Practice_1.java
@@ -1,5 +1,8 @@
 package Java_Concepts;
 
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
 public class Practice_1 {
 
 	static void p1() {
@@ -252,10 +255,227 @@ public class Practice_1 {
 
 //		16.To Change each word's first letter to upper case:
 
+		String s1 = "welcome to home";
+
+		String[] s2 = s1.split(" ");
+
+		String res = "";
+
+		for (String s : s2) {
+
+			char c = s.charAt(0);
+			String sub = s.substring(1);
+			res = res + Character.toUpperCase(c) + sub + " ";
+		}
+		System.out.println(res);
+
+	}
+
+	static void p18() {
+//		17.To remove character duplicates from String:
+
+		String s1 = "raining here...";
+
+		String res = "";
+
+		LinkedHashSet<Character> i = new LinkedHashSet<Character>();
+
+		for (int j = 0; j < s1.length(); j++) {
+			char c = s1.charAt(j);
+
+			i.add(c);
+		}
+
+		for (Character c : i) {
+			res = res + c;
+		}
+
+		System.out.println(res);
+	}
+
+	static void p19() {
+//		18.To remove word duplicates from a String:
+		String s1 = "java sql java python sql";
+
+		String res = "";
+
+		LinkedHashSet<String> i = new LinkedHashSet<String>();
+
+		String[] s2 = s1.split(" ");
+
+		for (String s : s2) {
+
+			i.add(s);
+
+		}
+
+		for (String c : i) {
+			res = res + c + " ";
+		}
+
+		System.out.println(res);
+	}
+
+	static void p20() {
+//		19.To remove the duplicates and to sort elements from Array:
+
+		int n[] = { 10, 20, 30, 40, 50, 10, 20, 30 };
+
+		LinkedHashSet<Integer> t = new LinkedHashSet<Integer>();
+//		TreeSet<Integer> t = new TreeSet<Integer>();
+
+		for (int i = 0; i < n.length; i++) {
+			t.add(n[i]);
+		}
+
+		System.out.println(t);
+	}
+
+	static void p21() {
+//		20.To sort numbers   in ascending order:
+
+		int arr[] = { 1, 2, 3, 4, 5, 6, 11, 21, 2, 3, 1, 7, 6, 5 };
+
+		for (int i = 0; i < arr.length; i++) {
+
+			for (int j = 0; j < arr.length; j++) {
+
+				if (arr[i] > arr[j]) {
+
+					int temp = arr[i];
+
+					arr[i] = arr[j];
+
+					arr[j] = temp;
+
+				}
+			}
+		}
+
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+
+	}
+
+	static void p22() {
+//	21.To sort elements in descending order:
+
+		int arr[] = { 1, 2, 3, 4, 5, 6, 11, 21, 2, 3, 1, 7, 6, 5 };
+
+		for (int i = 0; i < arr.length; i++) {
+
+			for (int j = 0; j < arr.length; j++) {
+
+				if (arr[i] < arr[j]) {
+
+					int temp = arr[i];
+
+					arr[i] = arr[j];
+
+					arr[j] = temp;
+
+				}
+			}
+		}
+
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+
+	}
+
+	static void p23() {
+
+//		22.To add each odd and even pair in an array: || not sure
+
+		int arr[] = { 10, 20, 30, 40, 50 };
+
+		try {
+
+			for (int i = 0; i < arr.length; i = i + 2) {
+
+				System.out.println(arr[i] + arr[i + 1]);
+
+			}
+		} catch (Exception e) {
+			System.out.println(arr[arr.length - 1]);
+		}
+
+	}
+
+	static void p24() {
+//		30.To check the given number is prime or not:
+
+		int count = 0;
+		int n = 7;
+
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				count++;
+			}
+		}
+
+		if (count == 2) {
+			System.out.println("Given number is prime");
+		} else {
+			System.out.println("Not prime");
+		}
+
+	}
+
+	static void p25() {
+//		31.Generate the given pattern:
+		/*
+		 * * ** *** ****
+		 * 
+		 */
+		int n = 5;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	static void p26() {
+//		32.Generate the given pattern:
+//			1
+//
+//			2 2
+//
+//			3 3 3
+
+		int n = 3;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+	static void p27() {
+//		33.Generate the given pattern:
+//			1
+//
+//			1 2
+//
+//			1 2 3
+
+		int n = 3;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
 	}
 
 	public static void main(String[] args) {
-		p17();
+		p27();
 	}
 
 }
